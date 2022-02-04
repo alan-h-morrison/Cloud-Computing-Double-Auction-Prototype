08992c984153084d6e649a439444085cb9447533
Alan Morrison <40400403@live.napier.ac.uk>
2022-02-04 15:28:27 +0000

    Implemmented simple navigation between main menu and settings menu

--- Cloud Computing Double Auction/Settings.xaml.cs
+++ Cloud Computing Double Auction/Settings.xaml.cs
@@ -10,31 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Cloud_Computing_Double_Auction
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Settings.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Settings : Window
     {
-        public MainWindow()
+        public Settings()
         {
             InitializeComponent();
-        }
-
-        private void BtnAuction_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void BtnSettings_Click(object sender, RoutedEventArgs e)
-        {
-            Settings settingsMenu = new Settings();
-            settingsMenu.Show();
-            this.Close();
         }
     }
 }
