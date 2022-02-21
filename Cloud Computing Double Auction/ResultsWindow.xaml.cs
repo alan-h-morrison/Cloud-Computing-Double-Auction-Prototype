40361dd0927bb59c00a13756df568416ce82abeb
Alan Morrison <40400403@live.napier.ac.uk>
2022-02-21 10:36:47 +0000

    Sizing of inital user bids data grid fixed

--- Cloud Computing Double Auction/ResultsWindow.xaml.cs
+++ Cloud Computing Double Auction/ResultsWindow.xaml.cs
@@ -26,15 +26,6 @@ namespace Cloud_Computing_Double_Auction
 
             DisplayParticipants();
             
-            foreach(DataGridColumn column in dgInitUserData.Columns)
-            {
-                //if you want to size your column as per the cell content
-                //column.Width = new DataGridLength(1.0, DataGridLengthUnitType.SizeToCells);
-                //if you want to size your column as per the column header
-                //column.Width = new DataGridLength(1.0, DataGridLengthUnitType.SizeToHeader);
-                //if you want to size your column as per both header and cell content
-                 column.Width = new DataGridLength(1.0, DataGridLengthUnitType.Auto);
-            }
         }
 
         public void DisplayParticipants()
